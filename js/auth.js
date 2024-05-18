27b9f1328100f62694bc47c419575da180ad9bad
robfil50219 <robfil50219@stud.noroff.no>
2024-05-18 11:04:54 +0200

    api key function

--- js/auth.js
+++ js/auth.js
@@ -8,3 +8,7 @@ async function login(email, password) {
     return await apiRequest('/social/auth/login', 'POST',{ email, password}); 
 }
 
+async function createApiKey(token) {
+    const response = await apiRequest('/social/auth/create-api-key', 'POST', null, token);
+}
+
