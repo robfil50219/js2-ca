ab3d965a415755132193317df6867432f06c3490
robfil50219 <robfil50219@stud.noroff.no>
2024-05-18 11:15:54 +0200

    fetch post function

---
+++ js/posts.js
@@ -0,0 +1,9 @@
+import { apiRequest } from "./api"; 
+
+async function fetchPosts(page = 1, limit = 10, sort = 'created', sortOrder = 'desc' , tag = ''){
+    const token = localStorage.getItem('token');
+    const apiKey= localStorage.getItem(apiKey); 
+    
+
+}
+
