b218d72382ab5aa5815ce698349a6b109280a576
robfil50219 <robfil50219@stud.noroff.no>
2024-05-18 11:18:41 +0200

    return await apiRequest

--- js/posts.js
+++ js/posts.js
@@ -3,7 +3,7 @@ import { apiRequest } from "./api";
 async function fetchPosts(page = 1, limit = 10, sort = 'created', sortOrder = 'desc' , tag = ''){
     const token = localStorage.getItem('token');
     const apiKey= localStorage.getItem(apiKey); 
-    
-
+    const endpoint = `/social/posts?page=${page}&limit=${limit}&sort=${sort}&sortOrder=${sortOrder}${tag ? `&_tag=${tag}` : ''}&_author=true&_comments=true&_reactions=true`;
+    return await apiRequest(endpoint,'GET', null, token, apiKey);
 }
 
