f8850209161c0ff8c1f075877644f78c741e38b4
robfil50219 <robfil50219@stud.noroff.no>
2024-05-18 10:53:17 +0200

    login authentication function

--- js/auth.js
+++ js/auth.js
@@ -3,3 +3,8 @@ import { apiRequest } from './api.js';
 async function register(email, password) {
     return await apiRequest('social/auth/register' , 'POST', { email, password }); 
 }
+
+async function login(email, password) {
+    return await apiRequest('/social/auth/login', 'POST',{ email, password}); 
+}
+
