ae9eb4348571ae543a553bc0f03af4020752450f
robfil50219 <robfil50219@stud.noroff.no>
2024-05-18 10:44:46 +0200

    export apiRequest

--- js/api.js
+++ js/api.js
@@ -12,3 +12,19 @@ async function apiRequest(endpoint, method = 'GET', body = null, token = null, a
     if (apiKey) {
         headers['x-api-key'] = apiKey;
     }
+
+    const response = await fetch(`${baseUrl}${endpoint}`, {
+        method,
+        headers,
+        body: body ? JSON.stringify(body) : null
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+        throw new Error(data.errors.map(error => error.message).join(', '));
+    }
+
+    return data;
+}
+
+export { apiRequest };
